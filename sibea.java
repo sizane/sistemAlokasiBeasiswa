320af4980d8625e5f5c38353b4d9af647dc0d767
natsuka07 <khairil1410.21.7@gmail.com>
2026-06-08 21:50:08 +0700

    1.0

---
+++ sibea.java
@@ -0,0 +1,21 @@
+package sistemAlokasiBeasiswaDanDanaBantuanKampus;
+
+public {
+    public Mahasigma(String nama, int gaji_ortu, int ukt, double ipk, double bebanUkt) {
+        this.nama = nama;
+        this.gaji_ortu = gaji_ortu;
+        this.ukt = ukt;
+        this.ipk = ipk;
+    };
+}
+
+class Mahasigma()
+
+/* 
+Cunny Love is cunny life, 
+one day I hope for a cunny wife as I arise in the morning I gaze upon my cunny wife yaninng, 
+with eyes like dazzling gems, and gems 10/10. 
+All I know all is well in life when I have myself a cunny wife
+*/
+
+/*dawg */
